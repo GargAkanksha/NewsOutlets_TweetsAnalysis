32badae64690b88247c26dcd294165545e667a4f
GargAkanksha <akanksha.30@gmail.com>
2018-07-09 16:25:13 -0500

    Commit consisting of files for Sentiments Analysis of News Outlets

---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = "B7VzQZmi7oqUudrfgEcLRUhhg"
+consumer_secret = "W2DWA01YJlsxK2EwyTODVZEzDP5SFPaIOhRJJD8N3mrXTn8LME"
+access_token = "1012413887533199360-skd0KmyU0praa9NEGnnPiAcga9uY5r"
+access_token_secret = "UrktFUnaR6oHE02F0LZKVJbDoLG6rcRvKQl7km6MpO2uO"
+
